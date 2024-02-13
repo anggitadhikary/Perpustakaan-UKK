c5cc66c6ae9fe3bdbeb6b90d963ff4cd2fcf8ca2
anggitadhikary <anggitadikari@gmail.com>
2024-02-13 15:03:19 +0700

    commit kedua

---
+++ resources/views/Layoutlanding/header.blade.php
@@ -0,0 +1,67 @@
+ <!--================ Start Header Menu Area =================-->
+ <header class="header_area">
+     <div class="main_menu">
+         <nav class="navbar navbar-expand-lg navbar-light">
+             <div class="container">
+                 <a class="navbar-brand logo_h" href="{{ url('/') }}"><img src="img/logo.png" alt=""></a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse"
+                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                     aria-label="Toggle navigation">
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                 </button>
+                 <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                     <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
+                         <li class="nav-item active"><a class="nav-link" href="{{ url('/') }}">Beranda</a></li>
+                         <li class="nav-item submenu dropdown">
+                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                 aria-haspopup="true" aria-expanded="false">Buku</a>
+                             <ul class="dropdown-menu">
+                                 <li class="nav-item"><a class="nav-link" href="{{ url('kategori') }}">List Buku</a>
+                                 </li>
+                             </ul>
+                         </li>
+
+                         <li class="nav-item submenu dropdown">
+                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                 aria-haspopup="true" aria-expanded="false">Pages</a>
+                             <ul class="dropdown-menu">
+                                 <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+                                 <li class="nav-item"><a class="nav-link" href="register.html">Register</a></li>
+                             </ul>
+                         </li>
+                         <li class="nav-item submenu dropdown">
+                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                 aria-haspopup="true" aria-expanded="false">Pages</a>
+                             <ul class="dropdown-menu">
+                                 {{-- @if (auth()->user()) --}}
+                                 <li class="nav-item">
+                                     <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
+                                 </li>
+                                 <li class="nav-item"> <a href="{{ url('profile') }}" class="nav-link">Profile</a>
+                                 </li>
+                                 {{-- @endif --}}
+
+
+                             </ul>
+                         </li>
+                     </ul>
+
+                     <ul class="nav-shop">
+
+
+
+                         {{-- @guest --}}
+
+                         <li class="nav-item"><a class="button button-header" href="">Login</a></li>
+
+                         <li class="nav-item"><a class="button button-header" href="">Register</a></li>
+                         {{-- @endguest --}}
+                     </ul>
+                 </div>
+             </div>
+         </nav>
+     </div>
+ </header>
+ <!--================ End Header Menu Area =================-->
---
+++ resources/views/Layoutlanding/template.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aroma Shop - Home</title>
+    <link rel="icon" href="img/Fevicon.png" type="image/png">
+    <link rel="stylesheet" href="{{ asset('vendors/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="{{ asset('vendors/themify-icons/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/nice-select/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/owl-carousel/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/owl-carousel/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/linericon/style.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+
+
+    @include('Layoutlanding.header')
+    @yield('content')
+    @include('Layoutlanding.footer')
+
+
+    <script src="{{ asset('vendors/jquery/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendors/skrollr.min.js') }}"></script>
+    <script src="{{ asset('vendors/owl-carousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('vendors/nice-select/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('vendors/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('vendors/mail-script.js') }}"></script>
+
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
