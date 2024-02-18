6cfc370954d050cdbd3be69003fb13ba21fb6cc4
anggitadhikary <anggitadikari@gmail.com>
2024-02-18 19:13:00 +0700

    commit ke ketujuh

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -211,13 +211,21 @@
                             <div class="review_box">
                                 <h4>Add a Review</h4>
                                 <p>Your Rating:</p>
-                                <ul class="list">
-                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
-                                </ul>
+                                <div class="rating-css">
+                                    <div class="star-icon">
+                                        <input type="radio" value="1" name="product_rating" checked
+                                            id="rating1">
+                                        <label for="rating1" class="fa fa-star"></label>
+                                        <input type="radio" value="2" name="product_rating" id="rating2">
+                                        <label for="rating2" class="fa fa-star"></label>
+                                        <input type="radio" value="3" name="product_rating" id="rating3">
+                                        <label for="rating3" class="fa fa-star"></label>
+                                        <input type="radio" value="4" name="product_rating" id="rating4">
+                                        <label for="rating4" class="fa fa-star"></label>
+                                        <input type="radio" value="5" name="product_rating" id="rating5">
+                                        <label for="rating5" class="fa fa-star"></label>
+                                    </div>
+                                </div>
                                 <p>Outstanding</p>
                                 <form action="#/" class="form-contact form-review mt-3">
                                     <div class="form-group">
