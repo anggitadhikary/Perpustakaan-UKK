b5cee265c68c05dc7c3725b69a842bba5e5330bf
anggitadhikary <anggitadikari@gmail.com>
2024-02-19 11:58:53 +0700

    commit 5

--- routes/web.php
+++ routes/web.php
@@ -26,17 +26,6 @@ Route::get('/buku-detail', function () {
     return view('product-detail');
 });
 
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
 Route::group(['middleware' => 'role:admin,petugas'], function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
